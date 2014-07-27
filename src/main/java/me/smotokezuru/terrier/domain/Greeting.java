b8cce9833811e81530a26f01122684444e096913
smotokezuru <hisami.shimizu@gmail.com>
2014-07-27 12:09:08 +0900

    Initial commit

---
+++ src/main/java/me/smotokezuru/terrier/domain/Greeting.java
@@ -0,0 +1,23 @@
+package me.smotokezuru.terrier.domain;
+
+/**
+ * Created by hisami on 2014/07/26.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/me/smotokezuru/terrier/mvc/GreetingController.java
@@ -0,0 +1,24 @@
+package me.smotokezuru.terrier.mvc;
+
+/**
+ * Created by hisami on 2014/07/26.
+ */
+import me.smotokezuru.terrier.domain.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
